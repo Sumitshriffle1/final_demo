0208e9965e180aa939d1ab4222afb98879066bf4
Sumitshriffle1 <sumitse@shriffle.com>
2023-09-14 18:21:20 +0530

    modified user in admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,11 +1,13 @@
 ActiveAdmin.register User do
-  permit_params :name, :email,:type
+  permit_params :name, :email,:password_digest,:contact,:type
 
   index do
     selectable_column
     id_column
     column :name
     column :email
+    column :password_digest
+    column :contact
     column :type
     actions
   end
@@ -13,6 +15,8 @@ ActiveAdmin.register User do
     f.inputs do
       f.input :name
       f.input :email
+      f.input :password_digest
+      f.input :contact
       f.input :type
     end
     f.actions
