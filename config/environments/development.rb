1f35fc9f721ca202e104824d84189dcd01de734a
Sumitshriffle1 <sumitse@shriffle.com>
2023-09-19 19:23:22 +0530

    updated successfully

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
     :port => 587,
-    :user_name => 'sumitse@shriffle.com',
-    :password  => "xkdyrznnwynojfqh",
+    :user_name => ENV['EMAIL'],
+    :password => ENV['EMAIL_PASSWORD'],
     :authentication => 'plain',
     :enable_starttls_auto => true
   }
